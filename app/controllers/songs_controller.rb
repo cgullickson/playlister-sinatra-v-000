6d5cb9bba8b0500d74b7010480f53c74facd06ca
Chris Gullickson <cgullickson5@gmail.com>
2018-01-25 18:11:28 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,11 @@
-class GenresController < ApplicationController
-   get '/genres' do
-     @genres = Genre.all
-     erb :'genres/index'
+class SongsController < ApplicationController
+   get '/songs' do
+     @songs = Song.all
+     erb :'songs/index'
    end
 
-   get '/genres/:slug' do
-     @genre = Genre.find_by_slug(params[:slug])
-     erb :'/genres/show'
+   get '/songs/:slug' do
+     @song = Song.find_by_slug(params[:slug])
+     erb :'/songs/show'
    end
 end
